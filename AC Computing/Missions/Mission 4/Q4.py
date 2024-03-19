9468a01f37dd3f5dde3bae5a88461dd6b137f853
Josh99bit <52200922+Josh99bit@users.noreply.github.com>
2024-03-19 10:27:33 +0800

    Update Q4.py

--- AC Computing/Missions/Mission 4/Q4.py
+++ AC Computing/Missions/Mission 4/Q4.py
@@ -1,20 +1,18 @@
 def max_stopping_time(n):
-    maximum = stopping_time(1)
-    number = 1
+    max=stopping_time(1)
+    pos=1
     for i in range(1, n+1):
-        stoppingTime = stopping_time(i)
-        if stoppingTime > maximum:
-            maximum = stoppingTime
-            num = i
-    return [num, maximum]
-
-
+        stoppingTime=stopping_time(i)
+        if stoppingTime>max:
+            max=stoppingTime
+            pos=i
+    return [pos,max]
 def max_max_num_reached(n):
-    maximum = max_num_reached(1)
-    num = 1
+    max=max_num_reached(1)
+    pos=1
     for i in range(1, n+1):
-        maximumNumReached = max_num_reached(i)
-        if maximumNumReached > maximum:
-            maximum = maximumNumReached
-            num = i
-    return [num, maximum]
+        m=max_num_reached(i)
+        if m>max:
+            max=m
+            pos=i
+    return [pos,max]

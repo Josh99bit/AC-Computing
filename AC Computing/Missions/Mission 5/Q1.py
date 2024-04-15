9c96d14df42146b626b3d35952dc96dd781115cb
Josh99bit <52200922+Josh99bit@users.noreply.github.com>
2024-04-15 14:01:58 +0800

    Update Q1.py

--- AC Computing/Missions/Mission 5/Q1.py
+++ AC Computing/Missions/Mission 5/Q1.py
@@ -5,7 +5,11 @@ def keyword_cipher(text, keyword):
         if char not in unique_keyword and char in alphabets:
             unique_keyword += char
 
-    remaining_letters = [letter for letter in alphabets if letter not in unique_keyword]
+    remaining_letters = []
+    for letter in alphabets:
+         if letter not in unique_keyword:
+             remaining_letters.append(letter)
+
     cipher_alphabet = list(unique_keyword) + remaining_letters
 
     result = ""
